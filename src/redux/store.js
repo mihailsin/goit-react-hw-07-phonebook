a16ea81fadc2cd923e9b34bb3f1060776b4c0cc5
mihailsin <mihailsin88@gmail.com>
2022-02-17 19:14:58 +0200

    removed unused variable

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { filterSlice } from './filter-slice';
 import { contactsApi } from './contactsApi';
 
